ef61f70d4d646fa02536eaf081724e3a25da845b
Surojit saha <surojitsaha2005@gmail.com>
2025-09-11 11:20:08 +0530

    Update series.sh (2018 section 2 8)

--- Years Practical/2018/series.sh
+++ Years Practical/2018/series.sh
@@ -1,15 +1,23 @@
-echo -n "Enter the number of term:"
+#!/bin/bash
+
+echo -n "Enter the number of term: "
 read n
+
+if [ "$n" -le 0 ]; then
+    echo "Error: Please enter a positive integer greater than zero."
+    exit 1
+fi
+
 sum=0
 
 for((i=1;i<=n;i++))
 do
-term=1
-
-	for((j=1;j<=i;j++))
-	do
-		term=$(echo "$term * $j"|bc)
-	done
-		sum=$(echo "scale=2;$sum + ($term/($i*$i))"|bc)
+    term=1
+    for((j=1;j<=i;j++))
+    do
+        term=$(echo "$term * $j" | bc)
+    done
+    sum=$(echo "scale=2; $sum + ($term/($i*$i))" | bc)
 done
+
 echo "The sum of the series is: $sum"
